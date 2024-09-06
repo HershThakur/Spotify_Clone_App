fbcff5aa11856e7dea599e3869edd007a88cb866
HTJoker <hersh.thakur@gmail.com>
2024-09-06 10:42:20 +0100

    added app colors

--- lib/core/config/theme/app_colors.dart
+++ lib/core/config/theme/app_colors.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
-  
+  static const primary = Color(0xff42C83C);
+  static const lightBg = Color(0xfff2f2f2);
+  static const darkBg = Color(0xff0D0C0C);
+  static const grey = Color(0xffBEBEBE);
+  static const darkgrey = Color(0xff343434);
 }

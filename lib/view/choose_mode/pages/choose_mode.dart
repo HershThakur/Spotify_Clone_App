ae7d743ef264519cf63cb522173242e168b21795
HTJoker <hersh.thakur@gmail.com>
2024-09-11 11:46:25 +0100

    added labels for differnt modes

--- lib/view/choose_mode/pages/choose_mode.dart
+++ lib/view/choose_mode/pages/choose_mode.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone/core/config/assets/app_images.dart';
+import 'package:spotify_clone/core/config/assets/app_vectors.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
@@ -6,7 +12,133 @@ class ChooseModePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello World"),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(40),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                colorFilter: ColorFilter.srgbToLinearGamma(),
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  AppImages.chooseModeBG,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                    width: 200,
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: const Color(0xff30393C).withOpacity(0.15),
+                              ),
+                              child: const Icon(
+                                Icons.dark_mode_outlined,
+                                size: 40,
+                                color: Color.fromARGB(220, 255, 255, 255),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        const Text(
+                          "Dark Mode",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(width: 40),
+                    Column(
+                      children: [
+                        ClipOval(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Container(
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: const Color(0xff30393C).withOpacity(0.15),
+                              ),
+                              child: const Icon(
+                                Icons.light_mode_outlined,
+                                size: 40,
+                                color: Color.fromARGB(220, 255, 255, 255),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        const Text(
+                          "Light Mode",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ChooseModePage(),
+                      ),
+                    );
+                  },
+                  title: "Get Started",
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

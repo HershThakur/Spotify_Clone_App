7c69643f9d70f4faa09ea00dc9bebda6dde77367
HTJoker <hersh.thakur@gmail.com>
2024-09-11 11:47:45 +0100

    refactored code and removed dead code

--- lib/view/intro/pages/get_started.dart
+++ lib/view/intro/pages/get_started.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/core/config/assets/app_images.dart';
+import 'package:spotify_clone/core/config/assets/app_vectors.dart';
 import 'package:spotify_clone/core/config/theme/app_colors.dart';
-
-import '../../../core/config/assets/app_vectors.dart';
+import 'package:spotify_clone/view/choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -15,7 +15,7 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-            padding: EdgeInsets.all(40),
+            padding: const EdgeInsets.all(40),
             decoration: const BoxDecoration(
               image: DecorationImage(
                 colorFilter: ColorFilter.srgbToLinearGamma(),
@@ -25,50 +25,58 @@ class GetStartedPage extends StatelessWidget {
                 ),
               ),
             ),
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: SvgPicture.asset(
-                      AppVectors.logo,
-                      width: 200,
-                    ),
-                  ),
-                  Spacer(),
-                  const Text(
-                    'Enjoy Listening To usic',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 21,
-                  ),
-                  const Text(
-                    'lorem a;lksdjfla;kjsd;fakjsd;fakjsd;fkajs;dlfkja;sdkfja;sldkfja;lskdjf;alksdjf;alksdjjf;aksdjjf;laksjdf;lakjsdf;llakjsdf;laksjdf;lkajsd;lkfj',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.grey,
-                      fontSize: 13,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  BasicAppButton(
-                    onPressed: () {},
-                    title: "Get Started",
-                  )
-                ],
-              ),
-            ),
           ),
           Container(
             color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(
+                    AppVectors.logo,
+                    width: 200,
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening To usic',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 21,
+                ),
+                const Text(
+                  'Magna ut aute officia consequat officia sint ea aute aliquip minim non in cupidatat officia. Tempor nulla laborum velit quis. Ea consectetur tempor veniam enim nulla',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 13,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => const ChooseModePage(),
+                      ),
+                    );
+                  },
+                  title: "Get Started",
+                )
+              ],
+            ),
           ),
         ],
       ),

c8705e34609950d98d037211e5dd9aa7630cdc16
HTJoker <hersh.thakur@gmail.com>
2024-09-13 09:54:20 +0100

    implementing auth section

---
+++ lib/view/authentication/pages/auth_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}

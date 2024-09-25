bc79d41fd0491660b94b538cf44de3953d078f4c
HTJoker <hersh.thakur@gmail.com>
2024-09-25 10:00:52 +0100

    setting up UI for Auth Page

--- lib/view/authentication/pages/auth_page.dart
+++ lib/view/authentication/pages/auth_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/core/config/assets/app_images.dart';
+import 'package:spotify_clone/core/config/assets/app_vectors.dart';
+import 'package:spotify_clone/core/config/theme/app_colors.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
@@ -8,7 +12,52 @@ class AuthPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(
+              AppVectors.topPattern,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(
+              AppVectors.bottomPattern,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(AppImages.authBG),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  AppVectors.logo,
+                ),
+                SizedBox(height: 55),
+                Text(
+                  "Enjoy Listening To Music",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 26,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  "Spotify is a proprietary Swedish audio streaming and media sevices provider",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 17,
+                    color: AppColors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );

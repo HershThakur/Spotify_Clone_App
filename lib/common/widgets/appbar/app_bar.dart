dc378e35b3fab0ec71b1212d73c5e563283cc8d4
HTJoker <hersh.thakur@gmail.com>
2024-10-02 15:52:01 +0100

    implemented title into appbar

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-class BasicAppBar extends StatelessWidget {
-  const BasicAppBar({super.key});
+class BasicAppBar extends StatelessWidget implements PreferredSizeWidget{
+  const BasicAppBar({super.key, this.title});
+
+  final Widget? title; 
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      title: title ?? const Text(""),
       backgroundColor: Colors.transparent,
       elevation: 0,
     );
   }
+  
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }

bdfa04c8df724cdc74508681690a95e34c23a148
HTJoker <hersh.thakur@gmail.com>
2024-10-09 11:40:48 +0100

    implemented basid appbar

--- lib/view/authentication/pages/signup_page.dart
+++ lib/view/authentication/pages/signup_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone/core/config/assets/app_vectors.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: BasicAppBar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: 40,
+        ),
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
   }
 }

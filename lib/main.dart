1f030431e964a9a293770d05ec9babad0634c4a0
HTJoker <hersh.thakur@gmail.com>
2024-11-13 10:09:00 +0000

    initilised firebase with project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone/core/config/theme/app_theme.dart';
+import 'package:spotify_clone/firebase_options.dart';
 import 'package:spotify_clone/view/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify_clone/view/splash/pages/splash.dart';
 
@@ -12,6 +14,9 @@ Future<void> main() async {
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory:
         kIsWeb ? HydratedStorage.webStorageDirectory : await getApplicationDocumentsDirectory(),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
